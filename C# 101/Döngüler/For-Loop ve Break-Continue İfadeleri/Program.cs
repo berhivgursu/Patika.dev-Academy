f9c1f69eec2bc960f5bb693161eb15f1f3ce6d66
berhiv gürsu <99619784+berhivgursu@users.noreply.github.com>
2024-07-27 03:48:35 +0300

    Add files via upload

---
+++ C# 101/Döngüler/For-Loop ve Break-Continue İfadeleri/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace For_Loop_ve_Break_Continue
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayı giriziniz:");
+            
+            int sayac = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= sayac; i++)
+            {
+                if (i % 2 == 1)
+                    Console.WriteLine(i);
+            }
+
+            int tekToplam = 0;
+            int ciftToplam = 0;
+            
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 2 == 1)
+                    tekToplam += i;
+                else
+                    ciftToplam += i;
+            }
+            Console.WriteLine("Tek Toplam:" + tekToplam);
+            Console.WriteLine("Çift Toplam:" + ciftToplam);
+
+            // Break-Continue
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                    break;
+                Console.WriteLine(i);
+            }
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                    continue;
+                Console.WriteLine(i);
+            }
+        }
+    } 
+}
---
+++ C# 101/Döngüler/While-Foreach/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace While_Foreach
+{
+    public class Program
+    {
+        static void Main()
+        {
+            // While
+
+            Console.WriteLine("Bir sayı giriniz:");
+
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam / sayi);
+
+            char character = 'a';
+            while (character <= 'z')
+            {
+                Console.Write(character);
+                character++;
+            }
+
+            // Foreach
+
+            string[] arabalar = {"BMW", "Ford", "Toyota", "Nissan"};
+            foreach(var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+    }
+}
