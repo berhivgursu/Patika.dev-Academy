7550c1bbbfcab9419e35ca95b34dd0d3e0a369e2
berhiv gürsu <99619784+berhivgursu@users.noreply.github.com>
2024-07-31 15:31:28 +0300

    Add files via upload

---
+++ C# 101/Diziler/Array Sınıfı Methodları/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Array_Sınfı_Methodları
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayiDizisi = {23, 12, 4, 86, 72, 3, 11, 17};
+
+            // Sırasız Dizi
+            foreach(var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+            
+            // Sıralı Dizi
+            Array.Sort(sayiDizisi);
+            foreach(var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+            
+            // Clear
+            Array.Clear(sayiDizisi, 2, 2);
+            foreach(var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+            // sayiDizisi elemanlarını kullanarak 2. indexten itibaren iki tane elemanı sıfırlar.
+            
+            // Reverse
+            Array.Reverse(sayiDizisi);
+            foreach(var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+            
+            // IndexOf
+            Console.WriteLine(Array.IndexOf(sayiDizisi, 23));
+
+            // Resize
+            Array.Resize<int>(ref sayiDizisi, 9);
+            sayiDizisi[8] = 99;
+            foreach(var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+        }
+    }
+}
