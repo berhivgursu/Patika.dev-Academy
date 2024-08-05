8310a5303fa1423ecf5a75758ed1677ca1d894f0
berhiv gürsu <99619784+berhivgursu@users.noreply.github.com>
2024-08-05 12:19:42 +0300

    Add files via upload

---
+++ Ödev 1/1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ex1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            uint n = uint.Parse(Console.ReadLine());
+            uint[] dizi = new uint[n];
+            
+            int i = 0;
+            while(i <= n - 1)
+            {
+                dizi[i] = uint.Parse(Console.ReadLine());
+                i++;
+            }
+
+            int length = dizi.Length;
+            for (int j = 0; j < length; j++)
+            {
+                if (dizi[j] % 2 == 0)
+                    Console.WriteLine(dizi[j]);
+            }
+        }
+    }
+}
---
+++ Ödev 1/4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ex_4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string cumle = Console.ReadLine();
+            
+            int harf_sayac = 0;
+            int kelime_sayac = 1;
+
+             for (int i = 0; i < cumle.Length; i++)
+            {
+                if (cumle[i] == ' ')
+                {
+                    kelime_sayac++;
+                }
+                else
+                {
+                    harf_sayac++;
+                }
+            }
+
+            Console.WriteLine("Harf Sayısı: " + harf_sayac);
+            Console.WriteLine("Kelime Sayısı: " + kelime_sayac);
+        }
+    }
+}
